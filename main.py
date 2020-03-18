5c65eaec87e9b22d5465c4081683e71c6f251723
Ivan Modric <bluesven869@gmail.com>
2020-03-18 09:20:23 +0800

    add comments

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import credentials
 import json
 import requests
 
+## check if logged in
 def isLoggin(driver):
     driver.get("https://disneyplus.com")
     isLogged = False
@@ -20,9 +21,13 @@ def isLoggin(driver):
 
     return isLogged
 
+## Login 
 def login(driver):
     print (" Sign In ")
     driver.get("https://disneyplus.com/login")
+
+    ## --------------- Type Email Address -------------------
+    # waits for email input box
     while True:
         try:
             emailInput = driver.find_element_by_name("email")
@@ -32,11 +37,16 @@ def login(driver):
         except:
             continue
     
+    # set email address to email field
     email = driver.find_element_by_name("email")
     email.clear()
     email.send_keys(credentials.email)
+
+    # click the submit button
     driver.find_element_by_name("dssLoginSubmit").click()
     
+    ## --------------- Type Password -------------------
+    # wait for password input box
     while True:
         try:
             passwordInput = driver.find_element_by_name("password")
@@ -45,29 +55,39 @@ def login(driver):
             continue
         except:
             continue
+    # set password to password field
     password = driver.find_element_by_name("password")
     password.clear()
     password.send_keys(credentials.password)
+
+    # click the submit button
     driver.find_element_by_name("dssLoginSubmit").click()
 
+## Click action for next arrow button
 def clickNextInSection(driver, element):
     driver.execute_script("arguments[0].scrollIntoView();", element)
     while True:
-        try:  
+        try:
+            # [1] is next arrow button, [0] is prev arrow button  
             element.find_elements_by_tag_name("button")[1].click()
             sleep(0.5)
             continue
         except:
             break
-    
+
+## Scan whole pages to get sections and movies (just Name and Image)    
 def pageScan(driver):
+    # Page Information Object
     pageInformation = None
+
+    # Read the data that was saved
     try:
         with open('temp.json') as f:
             pageInformation = json.load(f)
     except:
         pass
 
+    # waits for the @id[home-collection] div
     while True:
         try:
             homeCollection = driver.find_element_by_id("home-collection")
@@ -76,6 +96,7 @@ def pageScan(driver):
             continue
     sleep(5)
 
+    # if previous Page Information is valid, then skip to scan page.
     if pageInformation != None:
         return pageInformation
 
@@ -84,15 +105,23 @@ def pageScan(driver):
     lengthOfSections = len(divs) - 2
     lengthOfActives = len(divs[2].find_elements_by_class_name("slick-active"))
     i = 0
-    sections = []
+
+    sections = [] ## sections data
+
+    # div[0] => Slider 
+    # div[1] => Brands    
     for div in divs:
         i = i + 1
         if i < 3:
             continue
         
+        # click next arrow to show all videos
         clickNextInSection(driver, div)
+
+        # get Section Name and Images
         h4 = div.find_element_by_xpath(""".//h4""")
         sectionName = h4.get_attribute("innerHTML")
+
         imgs = div.find_elements_by_xpath(""".//img""")
         items = []
         for img in imgs:
@@ -101,10 +130,12 @@ def pageScan(driver):
                 "Image": img.get_attribute('src'),
                 "URL": ""
             })
+        
         sections.append({
             "Name": sectionName,
             "Items": items
         })
+
     print (" Page Scanning ... Done")
     return {
         "sectionsLen": lengthOfSections,
@@ -114,9 +145,13 @@ def pageScan(driver):
         "itemIndex": 0
     }
 
+## move to section, forexample - show the "Recommend For You" section to the clientview
 def moveToSection(driver, element):
     driver.execute_script("arguments[0].scrollIntoView();", element)
 
+## move to item (selected)
+#  itemIndex - 0, 1, 2, ...
+#  activeLen - 4 (count that is showed actively in specific section)
 def moveToItem(driver, element, itemIndex, activeLen):
     page = int(itemIndex / activeLen)
     
@@ -131,6 +166,7 @@ def moveToItem(driver, element, itemIndex, activeLen):
             print ("Error1234")
             continue
 
+##  Scrape Data - Get URL 
 def scrapeItem(driver, element, itemIndex):
     aTags = element.find_elements_by_xpath(""".//a""")
     aTag = aTags[itemIndex]
@@ -139,9 +175,8 @@ def scrapeItem(driver, element, itemIndex):
         actions.move_to_element_with_offset(aTag, 140, 90).perform()
         aTag.click()
     except Exception as e:
-        print("_DD_D_D_")
         print (e)
-
+        ## Close React Modal Overlay for the scrapping error.
         button = driver.find_element_by_xpath("""//div[@class=ReactModal__Overlay]""")
         button.click()
         aTag.click()
@@ -152,30 +187,42 @@ def scrapeItem(driver, element, itemIndex):
     driver.back()
     return movieLink
 
-
-def itemScan(driver, sectionIndex, itemIndex, activeLen):
+## Scan Item
+# sectionIndex - 0, 1, 2, ...
+# itemIndex - 0, 1, 2, ...
+# activeLen - length of active items (default 4)
+# sections - json object to store
+def itemScan(driver, sectionIndex, itemIndex, activeLen, sections):
     section = None
     sleep(2)
+    isSessionInvalid = False
     while True:
         try:
             divs = driver.find_elements_by_xpath("""//*[@id="home-collection"]/div/div""")
             section = divs[sectionIndex + 2]
             break
-        except Exception as e:
-            print("AAAA")
-            print(e)
+        except:
             continue
-    
+    if isSessionInvalid:
+        ## if session error, data should be saved.
+        with open('data.json', 'w') as f:
+            json.dump(sections, f, indent=4, sort_keys=False)
+
     URL = ''
+
     try:
+        ## get section name
         h4 = section.find_element_by_xpath(""".//h4""")
         sectionName = h4.get_attribute("innerHTML")
+        ## move to selected section
         moveToSection(driver, section)
-        print("MOVE TO SECTION DONE")
+        
+        ## move to selected Item
         moveToItem(driver, section, itemIndex, activeLen)
-        print("MOVE TO ITEM DONE")
+
+        ## scrape data for the selected item
         URL = scrapeItem(driver, section, itemIndex)
-        print ("URL", URL)
+
     except Exception as e:
         print (e)
     return URL
@@ -195,10 +242,10 @@ def main():
     pageInformations = pageScan(driver)
 
     sectionsLen = pageInformations['sectionsLen']
-    activeLen = pageInformations['activeLen']    
+    activeLen = pageInformations['activeLen']
     sections = pageInformations['sectionList']
-    sectionIndex = 4
-    itemIndex = 21
+    sectionIndex = pageInformations['sectionIndex']
+    itemIndex = pageInformations['itemIndex']
 
     ## click prev button for the first section
     divs = driver.find_elements_by_xpath("""//*[@id="home-collection"]/div/div""")
@@ -222,7 +269,6 @@ def main():
             sections[sectionIndex]['Items'][itemIndex - 1]['URL'] = URL
             print (json.dumps(sections[sectionIndex]['Items'][itemIndex - 1], indent=4))
             
-            print ("SectionsLen", sectionsLen, ", Section: ", sectionIndex, ", Item:", itemIndex, ", Length of Items:", len(sections[sectionIndex]['Items']) )
             if itemIndex >= len(sections[sectionIndex]['Items']):
                 sectionIndex = sectionIndex + 1
                 itemIndex = 0
@@ -235,12 +281,10 @@ def main():
             with open('temp.json', 'w') as f:
                 json.dump(pageInformations, f, indent=4, sort_keys=False)
         except Exception as e:
-            print('ERROR :::: ')
-            print(e)
             continue
         
     with open('data.json', 'w') as f:
-        json.dump(sections, f, indent=4, sort_keys=False)        
+        json.dump(sections, f, indent=4, sort_keys=False)
     sleep(5000)
     
 if __name__ == '__main__':
